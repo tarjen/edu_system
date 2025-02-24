6a88913ac839a617da3ee35ad5ae89d90942dfad
tarjen <2679818286@qq.com>
2025-02-25 01:19:04 +0800

    2025.2.25

--- online_judge/api/contest/show.py
+++ online_judge/api/contest/show.py
@@ -1,7 +1,9 @@
 from flask import Flask,jsonify
 from online_judge import app
 from online_judge.models.problems import Problem,Contest,ContestUser
+from online_judge.models.submissions import Submission
 import json
+
 @app.route('/api/contest/get_contest_list', methods=['GET'])
 def get_contest_list():
     contests = Contest.query.all()  # 从数据库中获取所有的 contest 数据
@@ -54,3 +56,62 @@ def get_contest_user_solved_problem(contest_id,user_id):
                 solved_problem.append(problem_id)
 
     return jsonify(solved_problem)
+
+@app.route('/api/contest/get_all_user/<int:contest_id>', methods=['GET'])
+def get_contest_all_user(contest_id):
+    contestuser = Contest.query.filter_by(id=contest_id).first()    
+    if contestuser is None:
+        return jsonify({"error": "contest or problem not found"}), 404    
+
+    contest_users = ContestUser.query.filter_by(contest_id=contest_id).all()
+    user_list = []
+    for user in contest_users:
+        user_list.append(user.user_id)
+    return jsonify(user_list)
+
+@app.route('/api/contest/get_all_submission/<int:contest_id>', methods=['GET'])
+def get_contest_all_submission(contest_id):
+    contestuser = Contest.query.filter_by(id=contest_id).first()    
+    if contestuser is None:
+        return jsonify({"error": "contest or problem not found"}), 404    
+
+    submissions = Submission.query.filter_by(contest_id=contest_id).all()
+    submission_list = []
+
+    for submission in submissions:
+        submission_data = {
+            'problem_id': submission.problem_id,
+            'user_id': submission.user_id,
+            'language': submission.language,
+            'submit_time': submission.submit_time,
+            'status': submission.status,
+            'time_used': submission.time_used,
+            'memory_used': submission.memory_used
+        }
+
+        submission_list.append(submission_data)
+
+    return jsonify(submission_list)
+
+@app.route('/api/contest/get_contest_user_submission/<int:contest_id>/<int:user_id>', methods=['GET'])
+def get_contest_user_solved_problem(contest_id,user_id):
+    contestuser = ContestUser.query.filter_by(contest_id=contest_id,user_id=user_id).first()    
+    if contestuser is None:
+        return jsonify({"error": "contest or problem not found"}), 404    
+
+    submissions = Submission.query.filter_by(contest_id=contest_id,user_id=user_id).all()
+    submission_list = []
+
+    for submission in submissions:
+        submission_data = {
+            'problem_id': submission.problem_id,
+            'submit_time': submission.submit_time,
+            'language': submission.language,
+            'status': submission.status,
+            'time_used': submission.time_used,
+            'memory_used': submission.memory_used
+        }
+
+        submission_list.append(submission_data)
+
+    return jsonify(submission_list)
